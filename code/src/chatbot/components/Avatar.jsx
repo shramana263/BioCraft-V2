5909c44894fc10713c6067d5f40ef2e850ed5832
shramana263 <shramana652@gmail.com>
2025-04-30 11:22:34 +0530

    feat: add Avatar component for displaying bot profile image

---
+++ code/src/chatbot/components/Avatar.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Avatar() {
+
+    return (
+        <div className='h-12 w-12 rounded-full overflow-hidden shadow-md' >
+            <img src="./src/assets/images/boticon.jpg" alt="" className='h-full w-full'/>
+        </div>
+    )
+}
