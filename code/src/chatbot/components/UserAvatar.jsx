d68d5b80575ee127e1c77bc1a1a0ee4c90f9a1a0
shramana263 <shramana652@gmail.com>
2025-05-05 11:22:34 +0530

    feat: add UserAvatar component for displaying user profile images

---
+++ code/src/chatbot/components/UserAvatar.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function UserAvatar() {
+
+    return (
+        <div className='h-12 w-12 rounded-full shadow-md overflow-hidden ms-3' >
+            <img src="./src/assets/images/user1.png" alt="" className='h-full w-full'/>
+        </div>
+    )
+}
